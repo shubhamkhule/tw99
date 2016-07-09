8819bcc01b2a39f15df53d8073be56895aff9e4e
shubham <shubhamaryans1@gmail.com>
2016-07-09 14:03:55 +0530

    first commit

---
+++ tw99.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : tw99.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,c;
+	cout<<"Enter any 2 numbers";
+	cin>>a>>b;
+	cout<<endl<<"Select the operation to be performed::";
+	cout<<endl<<"1.Addition";
+	cout<<endl<<"2.Subtraction";
+
+	return 0;
+}
